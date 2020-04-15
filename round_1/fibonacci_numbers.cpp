a3c4283b0ea0f15fb1ba6d6c619a8afa04da6c6b
Dmitry Komalov <dmitrykomalov@gmail.com>
2020-04-16 01:50:18 +0400

    init fibonacci

--- round_1/fibonacci_numbers.cpp
+++ round_1/fibonacci_numbers.cpp
@@ -1,1 +1,32 @@
 // https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+
+int fib(int n) {
+  map<int, int> memo = {
+    {1, 1},
+    {2, 1}
+  };
+
+  if (memo[n]) {
+    return memo[n];
+  }
+
+  int i = 3;
+  while (i <= n) {
+    memo[i] = memo[i - 1] + memo[i - 2];
+    i++;
+  }
+
+  return memo[n];
+}
+
+int main() {
+  int result = fib(40);
+  cout << result << endl;
+  return 1;
+}
