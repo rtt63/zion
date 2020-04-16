21632d1a1c31e997a42018ffeea65cb73c62064c
Dmitry Komalov <dmitrykomalov@gmail.com>
2020-04-17 01:00:13 +0400

    pairs

--- round_1/pairs.cpp
+++ round_1/pairs.cpp
@@ -1,1 +1,31 @@
 // https://www.hackerrank.com/challenges/pairs/problem
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// O(n^2)
+// can do better?
+int decision(vector<int> arr, int k)
+{
+  int counter = 0;
+
+  for (int i = 0; i < arr.size(); i++)
+  {
+    for (int j = i; j < arr.size(); j++)
+    {
+      if (abs(arr[i] - arr[j]) == k)
+      {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
+}
+
+int main()
+{
+  int result = decision({1, 2, 3, 4}, 1);
+  cout << result << endl;
+  return 0;
+}
