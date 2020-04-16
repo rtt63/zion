b9b4383b488496e564d199ca688ede120c8100f8
Dmitry Komalov <dmitrykomalov@gmail.com>
2020-04-16 22:23:37 +0400

    add idea for distant barcodes

--- round_1/distant_barcodes.cpp
+++ round_1/distant_barcodes.cpp
@@ -1,1 +1,20 @@
 // https://leetcode.com/problems/distant-barcodes/
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> mix(vector<int> v) {
+  // store to map
+  // use priority que
+  // return new vector
+
+  return {1};
+}
+
+int main()
+{
+  vector<int> first = mix({1,1,1,2,2,2});
+  vector<int> seconds = mix({1,1,1,1,2,2,3,3});
+  return 1;
+}
