bb02e036f4c1f453c32173da7f103bf24899e5f9
Dmitry Komalov <dmitrykomalov@gmail.com>
2020-04-17 20:28:33 +0400

    cycle linkedlist

--- round_1/find_cycle_in_a_linked_list.cpp
+++ round_1/find_cycle_in_a_linked_list.cpp
@@ -1,1 +1,22 @@
 // https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+    // edge cases suck
+    bool hasCycle(ListNode *head) {
+        ListNode* pointer = head;
+        ListNode* slow = pointer;
+        ListNode* fast = pointer->next->next;
+
+        while (slow && fast) {
+          if (slow == fast) {
+            return true;
+          } else {
+            slow = slow->next;
+            fast = fast->next->next;
+          }
+        }
+
+        return false;
+    }
+};
