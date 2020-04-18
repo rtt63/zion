902265a282254071164b625d81df3c4831c9da49
Dmitry Komalov <dmitrykomalov@gmail.com>
2020-04-18 20:17:45 +0400

    bullshit upd

--- round_1/number_of_enclaves.cpp
+++ round_1/number_of_enclaves.cpp
@@ -1,1 +1,43 @@
 // https://leetcode.com/problems/number-of-enclaves/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print(vector<int> v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << ", ";
+  }
+  cout << "" << endl;
+}
+
+int c(int x, int y, vector<vector<int>> v) {
+  if (x < 0 || y < 0 || y > (v.size() - 1) || x > (v[1].size() - 1) || v[x][y] == 0) {
+    return 0;
+  }
+  return 1 + c(x - 1, y, v) + c(x + 1, y, v);
+}
+
+int num_enclaves(vector<vector<int>> v) {
+  vector<int> values;
+
+  for (int i = 0; i < v.size(); i++) {
+    for (int j = 0; j < v[i].size(); j++) {
+      int value = c(i, j, v);
+      cout << value << endl;
+      values.push_back(value);
+    }
+    cout << "" << endl;
+  }
+  //
+  // print(values);
+
+  return 1;
+}
+
+int main() {
+  vector<vector<int>> v{{0,0,0,0},{1,0,1,0},{0,1,1,0},{0,0,0,0}};
+  int result = num_enclaves(v);
+  cout << result << endl;
+  return 1;
+}
--- round_1/redundant_connection.cpp
+++ round_1/redundant_connection.cpp
@@ -1,1 +1,39 @@
 // https://leetcode.com/problems/redundant-connection/
+
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+void print(vector<int> v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << ", ";
+  }
+  cout << "" << endl;
+}
+
+// find cycle in graph, NOT DONE YET
+vector<int> findRedundantConnection(vector<vector<int>> edges) {
+  set<int> visited;
+  vector<int> result;
+  for (int i = 0; i < edges.size(); i++) {
+    vector<int> cur = edges[i];
+    int n = cur[1];
+    auto search = visited.find(n);
+    if (search != visited.end()) {
+      result = cur;
+      break;
+    } else {
+      visited.insert(n);
+    }
+  }
+  return result;
+}
+
+int main() {
+  vector<vector<int>> m {{1,4},{3,4},{1,3},{1,2},{4,5}};
+  vector<int> r = findRedundantConnection(m);
+  print(r);
+  cout << "[1,3]" << endl;
+  return 1;
+}
