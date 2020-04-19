cac68321e1023c0d08831932b3374b48dc83a2b1
Dmitry Komalov <dmitrykomalov@gmail.com>
2020-04-20 02:02:56 +0400

    complete redundant connection

--- round_1/redundant_connection.cpp
+++ round_1/redundant_connection.cpp
@@ -2,12 +2,89 @@
 
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
 using namespace std;
 
+void print(std::vector<int> nodes) {
+  for (auto& node : nodes) {
+    std::cout << node << ", ";
+  }
+  std::cout << "" << std::endl;
+}
+
+class DSU {
+  std::vector<int> nodes_ids;
+  std::vector<int> sets_sizes;
+
+  public:
+    DSU(int n) {
+      nodes_ids = std::vector<int>(n);
+      sets_sizes = std::vector<int>(n);
+
+      for (int i = 0; i < nodes_ids.size(); i++) {
+        nodes_ids[i] = i; // link to itself, like separated nodes in a fucking space
+        sets_sizes[i] = 1; // each node is a fucking set of one
+      }
+    }
+
+    int find(int id) {
+      int root = id;
+      while (nodes_ids[root] != root) {
+        root = nodes_ids[root];
+      }
+
+      // compress paths
+      // all nodes in set should directly point
+      // to it's own root
+      while (id != root) {
+        int next = nodes_ids[id];
+        nodes_ids[id] = root;
+        id = next;
+      }
+
+      return root;
+    }
+
+    bool connected(int a, int b) {
+      return find(a) == find(b);
+    }
+
+    void unify(int a, int b) {
+      if (connected(a, b)) {
+        return;
+      }
+
+      int root1 = find(a);
+      int root2 = find(b);
+
+      if (sets_sizes[root1] < sets_sizes[root2]) {
+        sets_sizes[root2] += sets_sizes[root1];
+        for (int i = 0; i < nodes_ids.size(); i++) {
+          if (nodes_ids[i] == root1) {
+            nodes_ids[i] = root2;
+          }
+        }
+      } else {
+        sets_sizes[root1] += sets_sizes[root2];
+        for (int i = 0; i < nodes_ids.size(); i++) {
+          if (nodes_ids[i] == root2) {
+            nodes_ids[i] = root1;
+          }
+        }
+      }
+    }
+};
+
 vector<int> findRedundantConnection(vector<vector<int>> edges) {
-  // union find
+  DSU dsu(6000);
+  for (int i = 0; i < edges.size(); i++) {
+    int l = edges[i][0];
+    int r = edges[i][1];
+    if (dsu.connected(l, r)) {
+      return edges[i];
+    } else {
+      dsu.unify(l, r);
+    }
+  }
 }
 
 int main() {
@@ -18,3 +95,8 @@ int main() {
   cout << "[1,3]" << endl;
   return 1;
 }
+
+/*
+Runtime: 184 ms, faster than 5.18% of C++ online submissions for Redundant Connection.
+Memory Usage: 9.8 MB, less than 100.00% of C++ online submissions for Redundant Connection.
+*/
